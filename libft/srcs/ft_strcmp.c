dcbfbc51b2b4e83938c4ecf943ee30fe90bfd6c2
GabrielPora <gabriel.groener@gmail.com>
2016-08-15 17:04:20 +0200

    Normed libaray files

--- libft/srcs/ft_strcmp.c
+++ libft/srcs/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/10 14:50:28 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/06/30 07:52:13 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/08/15 17:01:04 by ggroener         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,6 @@ int	ft_strcmp(const char *s1, const char *s2)
 		c2 = (unsigned char)s2[i];
 	}
 	if (c1 != c2)
-		return(c1 - c2);
+		return (c1 - c2);
 	return (0);
 }
