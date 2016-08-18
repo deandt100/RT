9a21acfd6731454e4b48248552f0c8edf1a3f4fd
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-08-18 15:55:09 +0200

    working on cone normals

--- srcs/vector_project.c
+++ srcs/vector_project.c
@@ -1,26 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector_dir.c                                       :+:      :+:    :+:   */
+/*   vector_project.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/08/15 09:05:27 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/08/18 13:44:25 by ddu-toit         ###   ########.fr       */
+/*   Created: 2016/08/18 08:56:35 by ddu-toit          #+#    #+#             */
+/*   Updated: 2016/08/18 09:24:00 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-** Function takes start vector (s) and a point (t) as a direction vector and returns
-** normalized direction vector.
-*/
-
 #include <rtv1.h>
 
-t_vector	vector_dir(t_vector *s, t_vector *t)
-{
-	t_vector	ret;
+/*
+** Project vector a onto vector b, producing a new vector returned by function.
+*/
 
-	ret = vector_sub(t, s);
-	return (vector_norm(&ret));
+t_vector	vector_project(t_vector a, t_vector b)
+{
+	return (vector_scale(vector_dot(&a, &b) / vector_dot(&b , &b), &b));
 }
