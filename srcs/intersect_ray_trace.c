7a730b71736360b88ce2e7db26b069fbc20a8bc9
Dean DU TOIT <ddu-toit@e5r7p3.jnb.42.fr>
2016-08-22 13:05:56 +0200

    added ambient

--- srcs/intersect_ray_trace.c
+++ srcs/intersect_ray_trace.c
@@ -6,7 +6,7 @@
 /*   By: ddu-toit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/08 14:02:18 by ddu-toit          #+#    #+#             */
-/*   Updated: 2016/08/21 14:19:11 by ddu-toit         ###   ########.fr       */
+/*   Updated: 2016/08/22 08:45:07 by ddu-toit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,13 +29,13 @@ int			intersect_ray_cone(t_ray *ray, t_cone *cone, float *t)
 		SQR(vector_dot(cone->v, V));
 	del_p = vector_sub(P, cone->p);
 	b = vector_sub(del_p, vector_scale(vector_dot(del_p, cone->v), cone->v));
-	rs.b = 2.0f * SQR(cos(cone->alpha)) * vector_dot(a, b) - 2.0f
+	rs.b = 2.0F * SQR(cos(cone->alpha)) * vector_dot(a, b) - 2.0F
 		* SQR(sin(cone->alpha))
 		* vector_dot(V, cone->v) * vector_dot(del_p, cone->v);
 	rs.c = SQR(cos(cone->alpha)) * VEC_SQR(b) - SQR(sin(cone->alpha)) *
 		SQR(vector_dot(del_p, cone->v));
 	rs.discr = SQR(rs.b) - (4 * rs.a * rs.c);
-	if (rs.discr < 0)
+	if (rs.discr < 0.0F)
 		return (0);
 	else
 		return (solve_quadratic(&rs, t));
@@ -57,10 +57,10 @@ int			intersect_ray_cylinder(t_ray *ray, t_cylinder *cyl, float *t)
 	rs.a = VEC_SQR(a);
 	del_p = vector_sub(P, PA);
 	b = vector_sub(del_p, vector_scale(vector_dot(del_p, VA), VA));
-	rs.b = 2.0f * vector_dot(a, b);
+	rs.b = 2.0F * vector_dot(a, b);
 	rs.c = VEC_SQR(b) - SQR(cyl->radius);
 	rs.discr = SQR(rs.b) - (4 * rs.a * rs.c);
-	if (rs.discr < 0)
+	if (rs.discr < 0.0F)
 		return (0);
 	else
 		return (solve_quadratic(&rs, t));
@@ -80,7 +80,7 @@ int			intersect_ray_sphere(t_ray *ray, t_sphere *sphere, float *t)
 	rs.b = 2 * vector_dot(ray->dir, rs.dist);
 	rs.c = vector_dot(rs.dist, rs.dist) - (sphere->radius * sphere->radius);
 	rs.discr = rs.b * rs.b - (4 * rs.a * rs.c);
-	if (rs.discr < EPSILON)
+	if (rs.discr < 0.0F)
 		return (0);
 	else
 		return (solve_quadratic(&rs, t));
