8a1f5f14750ce891ad078c7a9b5d30b5adbb1e3b
Owen EXALL <oexall@e5r6p1.jnb.42.fr>
2016-08-26 15:49:44 +0200

    Drawing a MONKEY

--- srcs/intersect_object.c
+++ srcs/intersect_object.c
@@ -6,13 +6,13 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/26 08:51:30 by oexall            #+#    #+#             */
-/*   Updated: 2016/08/26 14:58:54 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/26 15:41:27 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <rtv1.h>
 
-int		intersect_ray_obj(t_ray *ray, t_object *obj, double *t, int face)
+/*int		intersect_ray_obj(t_ray *ray, t_object *obj, double *t, int face)
 {
 	t_ray_tri	r;
 
@@ -39,7 +39,7 @@ int		intersect_ray_obj(t_ray *ray, t_object *obj, double *t, int face)
 	*t = r.tmp - 0.05F;
 //	obj->normals[obj->faces[face].nor] = vector_cross(r.e1, r.e2);
 	return (1);
-}
+}*/
 
 void	empty_others(t_env *env)
 {
@@ -52,41 +52,43 @@ void	empty_others(t_env *env)
 
 void	set_val_object(t_env *env, double t, t_ray ray)
 {
-	//(void)t;
-	//(void)ray;
 	OBJ.new_start = vector_add(ray.start, vector_scale(t, ray.dir));
-	OBJ.cur_mat = OBJ.mats[OBJ.objects[OBJ.cur_object].material];
+	OBJ.normal = OBJ.objects[OBJ.cur_object[0]].faces[OBJ.cur_object[1]].normal;
+	OBJ.normal = vector_scale(1.0f / ABSV(OBJ.normal), OBJ.normal);
+	vector_norm(&OBJ.normal);
+	if (vector_dot(ray.dir, OBJ.normal) > 0.0F)
+		OBJ.normal = vector_sub((t_vector){0, 0, 0}, OBJ.normal);
+	OBJ.cur_mat = OBJ.mats[OBJ.objects[OBJ.cur_object[0]].material];
 }
 
 void	gi_object(t_env *env, t_ray *ray, double *t, double *ref_dist)
 {
-	int		i;
-	int		j;
-	double	t6;
+	int			i;
+	int			j;
+	double		t6;
+	t_vector	n;
 
 	t6 = *t;
 	i = -1;
-	OBJ.cur_object = -1;
+	OBJ.cur_object[0] = -1;
+	OBJ.cur_object[1] = -1;
 	while (++i < OBJ.num_objects)
 	{
 		j = -1;
 		while (++j < OBJ.objects[i].num_faces)
-			if (intersect_ray_obj(ray, &OBJ.objects[i], &t6, j))
+			if (intersect_ray_tri(ray, &OBJ.objects[i].faces[j], &t6, &n))
 			{
-				OBJ.objects[i].dist = vector_dist(vector_add(ray->start,
-							vector_scale(t6, ray->dir)), ray->start);
-				if (OBJ.objects[i].dist < *ref_dist)
+				OBJ.objects[i].faces[j].shape.dist = vector_dist(vector_add(
+					ray->start, vector_scale(t6, ray->dir)), ray->start);
+				if (OBJ.objects[i].faces[j].shape.dist < *ref_dist)
 				{
 					*t = t6;
-					*ref_dist = OBJ.objects[i].dist;
-					OBJ.cur_object = i;
+					*ref_dist = OBJ.objects[i].faces[j].shape.dist;
+					OBJ.cur_object[0] = i;
+					OBJ.cur_object[1] = j;
+					OBJ.objects[i].faces[j].normal = n;
 					empty_others(env);
 				}
-				OBJ.normal = OBJ.objects[i].normals[OBJ.objects[i].faces[j].nor];
-//				OBJ.normal = vector_scale(1.0f / ABSV(OBJ.normal), OBJ.normal);
-				if (vector_dot(ray->dir, OBJ.normal) > 0.0F)
-					OBJ.normal = vector_sub((t_vector){0, 0, 0}, OBJ.normal);
-//				vector_norm(&OBJ.normal);
 			}
 	}
 }
--- srcs/read_obj.c
+++ srcs/read_obj.c
@@ -6,7 +6,7 @@
 /*   By: ggroener <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/26 08:57:43 by ggroener          #+#    #+#             */
-/*   Updated: 2016/08/26 13:43:40 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/26 15:45:13 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,38 +31,34 @@ void		create_obj_arrays(t_object *obj)
 {
 	obj->vectors = (t_vector *)malloc(sizeof(t_vector) * obj->num_vecs);
 	obj->normals = (t_vector *)malloc(sizeof(t_vector) * obj->num_norms);
-	obj->faces = (t_face *)malloc(sizeof(t_face) * obj->num_faces);
+	obj->faces = (t_triangle *)malloc(sizeof(t_triangle) * obj->num_faces);
 	obj->count.vecs = -1;
 	obj->count.norms = -1;
 	obj->count.faces = -1;
 }
 
-t_face		ft_face(char *line)
+t_triangle	ft_face(char *line, t_object *obj)
 {
-	t_face	face;
-	char	*tmp;
+	t_triangle	triangle;
+	char		*tmp;
 
 	tmp = ft_strchr(line, ' ');
-	face.v1 = ft_atoi(tmp) - 1;
+	triangle.v1 = obj->vectors[ft_atoi(tmp) - 1];
 	tmp = ft_strchr(&tmp[1], '/');
 	while (*tmp == '/')
 		tmp++;
-	face.nor = ft_atoi(tmp) - 1;
+	triangle.normal = obj->normals[ft_atoi(tmp) - 1];
 	tmp = ft_strchr(tmp, ' ');
-	face.v2 = ft_atoi(tmp) - 1;
+	triangle.v2 = obj->vectors[ft_atoi(tmp) - 1];
 	tmp = ft_strchr(&tmp[1], '/');
 	while (*tmp == '/')
 		tmp++;
-	if (ft_atoi(tmp) - 1 != face.nor)
-		exit(-1);
 	tmp = ft_strchr(tmp, ' ');
-	face.v3 = ft_atoi(tmp) - 1;
+	triangle.v3 = obj->vectors[ft_atoi(tmp) - 1];
 	tmp = ft_strchr(&tmp[1], '/');
 	while (*tmp == '/')
 		tmp++;
-	if (ft_atoi(tmp) - 1 != face.nor)
-		exit(-1);
-	return (face);
+	return (triangle);
 }
 
 void		read_obj(t_object *obj)
@@ -83,22 +79,22 @@ void		read_obj(t_object *obj)
 		else if (line[0] == 'v' && line[1] == 'n' && line[2] == ' ')
 			obj->normals[(++obj->count.norms)] = ft_obj_vector(line);
 		else if (line[0] == 'f' && line[1] == ' ')
-			obj->faces[(++obj->count.faces)] = ft_face(line);
+			obj->faces[(++obj->count.faces)] = ft_face(line, obj);
 		free(line);
 	}
 	close(fd);
 	
 	//DEBUG
 	int	i;
-	//for (i = 0; i < obj->num_vecs; i++)
-	//	print_vector("v", obj->vectors[i]);
-	//for (i = 0; i < obj->num_norms; i++)
-	//	print_vector("vn", obj->normals[i]);
+	for (i = 0; i < obj->num_vecs; i++)
+		print_vector("v", obj->vectors[i]);
+	for (i = 0; i < obj->num_norms; i++)
+		print_vector("vn", obj->normals[i]);
 	for (i = 0; i < obj->num_faces; i++)
-		printf("%i\t%i\t%i\tN: %i\n",
-			obj->faces[i].v1,
-			obj->faces[i].v2,
-			obj->faces[i].v3,
-			obj->faces[i].nor);
+	{
+		print_vector("V1", obj->faces[i].v1);
+		print_vector("V2", obj->faces[i].v2);
+		print_vector("V3", obj->faces[i].v3);
+	}
 	//END
 }
